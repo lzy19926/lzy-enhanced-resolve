0a03b19eef6a52c9c88b45485b60bb803ce4240c
阳九 <871921282@qq.com>
2023-04-20 00:35:01 +0800

    整理resolver流转逻辑,正确触发最终回调,获取结果

--- test/index.js
+++ test/index.js
@@ -1,6 +1,33 @@
-const resolve = require('../lib/index')
+const { resolve, createResolver } = require('../lib/index')
 
 
-resolve("/some/path/to/folder", "module/dir", (err, result) => {
-    console.log('plugins流转完毕');
-});
+// ------------默认resolve方法测试-----------
+//TODO 此时传入的回调会在resultPlugin中执行
+function resolve_Test() {
+    resolve("/some/path/to/folder", "module/dir", (err, result) => {
+        console.log('plugins流转完毕', err, result);
+    });
+}
+
+// ------------通过配置创建resolver,执行相关方法测试-----------
+function createResolver_Test() {
+    const defaultOptions = {
+        descriptionFiles: ["package.json"],
+        extensions: [".js", ".json", ".node"],
+        indexFiles: ["index"],
+        mainFields: ["main"],
+        fileSystem: undefined
+    }
+
+
+    const resolver = createResolver(defaultOptions)
+
+    resolver.resolve("/some/path/to/folder", "module/dir", (err, result) => {
+        console.log('plugins流转完毕', err, result);
+    });
+}
+
+
+// ---------执行测试------------
+// resolve_Test()
+createResolver_Test()
